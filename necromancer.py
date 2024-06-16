ea87a9ca31e47d636cb89e648389c5a56f21caf2
EDHJH <huang_ed@student.kings.edu.au>
2024-06-17 08:57:01 +1000

    CANGES

--- necromancer.py
+++ necromancer.py
@@ -1,6 +1,6 @@
 from character import Character
 
-class Mage(Character):
+class Necromancer(Character):
     def __init__(self, name):
-        super().__init__(name, "Mage", armour = 5)
+        super().__init__(name, "Necromancer", armour = 5)
         # Additional attributes and methods specific to the Mage class
--- ranger.py
+++ ranger.py
@@ -11,26 +11,7 @@ class Ranger(Character):
         self.current_hp = max_hp
         self.attacks = {
             "Arrow Shot": {"method": self.arrow_shot, "stamina_cost": 10},
-            "Charged Arrow": {"method": self.charge, "stamina_cost": 20},
-            "Lightning Arrow": {"method": self.charge, "stamina_cost": 25},
-            
-        }
-
-
-
-class Warrior(Character):
-    def __init__(self, name, max_hp):
-        super().__init__(name, "Warrior", armor=10)
-        self.max_stamina = 100
-        self.current_stamina = self.max_stamina
-        self.stamina_regeneration = 10
-        self.strength = 15
-        self.max_hp = max_hp
-        self.current_hp = max_hp
-        self.attacks = {
-            "Basic Attack": {"method": self.basic_attack, "stamina_cost": 10},
-            "Charge": {"method": self.charge, "stamina_cost": 20},
-            "Cleave Attack": {"method": self.cleave_attack, "stamina_cost": 30},
-            "Shield Bash": {"method": self.shield_bash, "stamina_cost": 15},
-            "Defensive Stance": {"method": self.defensive_stance, "stamina_cost": 5},
+            "Charged Arrow": {"method": self.charged_arrow, "stamina_cost": 20},
+            "Lightning Arrow": {"method": self.lightning_arrow, "stamina_cost": 25},
+            "Rain of Arrows": {"method": self.rain_of_arrows, "stamina_cost": 50}
         }
